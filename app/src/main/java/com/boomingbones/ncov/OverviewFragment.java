0e0b73a39f5893d703ca575f9b0dbaedccaa6271
BoomingBones <815067099@qq.com>
2020-06-30 16:19:18 +0800

    改进疫情总览Fragment的UI设计和初始化代码逻辑

--- app/src/main/java/com/boomingbones/ncov/OverviewFragment.java
+++ app/src/main/java/com/boomingbones/ncov/OverviewFragment.java
@@ -6,13 +6,18 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.text.SpannableStringBuilder;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
 
@@ -25,8 +30,6 @@ public class OverviewFragment extends Fragment {
 
     private View view;
     private Context context;
-    private LinearLayout itemContainer_domestic;
-    private LinearLayout itemContainer_foreign;
 
     public OverviewFragment() {
         // Required empty public constructor
@@ -39,6 +42,66 @@ public class OverviewFragment extends Fragment {
         // Inflate the layout for this fragment
         view  = inflater.inflate(R.layout.fragment_overview, container, false);
         context = getContext();
+
+        View domestic = LayoutInflater.from(context)
+                .inflate(R.layout.fragment_overview_overall_domestic, container, false);
+        setTextViewText(Data.overallDomesticCurConfirmedCount_incr,
+                (TextView) domestic.findViewById(R.id.curConfirmedIncr_text),
+                R.color.curConfirmed);
+        setTextViewText(Data.overallDomesticImportedCount_incr,
+                (TextView) domestic.findViewById(R.id.importedIncr_text),
+                R.color.imported);
+        setTextViewText(Data.overallDomesticSuspectedCount_incr,
+                (TextView) domestic.findViewById(R.id.suspectedIncr_text),
+                R.color.suspected);
+        setTextViewText(Data.overallDomesticConfirmedCount_incr,
+                (TextView) domestic.findViewById(R.id.confirmedIncr_text),
+                R.color.confirmed);
+        setTextViewText(Data.overallDomesticDeadCount_incr,
+                (TextView) domestic.findViewById(R.id.deathIncr_text),
+                R.color.death);
+        setTextViewText(Data.overallDomesticCuredCount_incr,
+                (TextView) domestic.findViewById(R.id.curedIncr_text),
+                R.color.cured);
+        ((TextView) domestic.findViewById(R.id.curConfirmedCount_text))
+                .setText(addComma(Data.overallDomesticCurConfirmedCount));
+        ((TextView) domestic.findViewById(R.id.importedCount_text))
+                .setText(addComma(Data.overallDomesticImportedCount));
+        ((TextView) domestic.findViewById(R.id.suspectedCount_text))
+                .setText(addComma(Data.overallDomesticSuspectedCount));
+        ((TextView) domestic.findViewById(R.id.confirmedCount_text))
+                .setText(addComma(Data.overallDomesticConfirmedCount));
+        ((TextView) domestic.findViewById(R.id.deathCount_text))
+                .setText(addComma(Data.overallDomesticDeadCount));
+        ((TextView) domestic.findViewById(R.id.curedCount_text))
+                .setText(addComma(Data.overallDomesticCuredCount));
+        FrameLayout overallContainer_domestic = view.findViewById(R.id.overall_container_domestic);
+        overallContainer_domestic.addView(domestic);
+
+        View foreign = LayoutInflater.from(context)
+                .inflate(R.layout.fragment_overview_overall_foreign, container, false);
+        setTextViewText(Data.overallForeignCurConfirmedCount_incr,
+                (TextView) foreign.findViewById(R.id.curConfirmedIncr_text),
+                R.color.curConfirmed);
+        setTextViewText(Data.overallForeignConfirmedCount_incr,
+                (TextView) foreign.findViewById(R.id.confirmedIncr_text),
+                R.color.confirmed);
+        setTextViewText(Data.overallForeignDeadCount_incr,
+                (TextView) foreign.findViewById(R.id.deathIncr_text),
+                R.color.death);
+        setTextViewText(Data.overallForeignCuredCount_incr,
+                (TextView) foreign.findViewById(R.id.curedIncr_text),
+                R.color.cured);
+        ((TextView) foreign.findViewById(R.id.curConfirmedCount_text))
+                .setText(addComma(Data.overallForeignCurConfirmedCount));
+        ((TextView) foreign.findViewById(R.id.confirmedCount_text))
+                .setText(addComma(Data.overallForeignConfirmedCount));
+        ((TextView) foreign.findViewById(R.id.deathCount_text))
+                .setText(addComma(Data.overallForeignDeadCount));
+        ((TextView) foreign.findViewById(R.id.curedCount_text))
+                .setText(addComma(Data.overallForeignCuredCount));
+        FrameLayout overallContainer_foreign = view.findViewById(R.id.overall_container_foreign);
+        overallContainer_foreign.addView(foreign);
 
         String[][] tempDataList1 = {
                 Arrays.copyOf(Data.domesticProvinceNameList, OVERVIEW_ITEM_COUNT),
@@ -62,7 +125,27 @@ public class OverviewFragment extends Fragment {
                     tempDataList2[3][i], tempDataList2[4][i], itemContainer2);
         }
 
+        Log.w("TEXT", ((TextView) foreign.findViewById(R.id.curedCount_text)).getText().toString());
+
         return view;
+    }
+
+    private void setTextViewText(String increment, TextView textView, int colorId) {
+        if (increment.equals("0")) {
+            textView.setText("较昨日无变化");
+        } else {
+            String text;
+            if (Integer.valueOf(increment) > 0) {
+                text = "较昨日+" + addComma(increment);
+            } else {
+                text = "较昨日" + addComma(increment);
+            }
+
+            SpannableString spannableString = new SpannableString(text);
+            ForegroundColorSpan colorSpan = new ForegroundColorSpan(getResources().getColor(colorId));
+            spannableString.setSpan(colorSpan, 3, text.length(), Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+            textView.setText(spannableString);
+        }
     }
 
     private void addItem(String areaName, String curConfirmedCount, String confirmedCount,
@@ -77,5 +160,10 @@ public class OverviewFragment extends Fragment {
         ((TextView) item.findViewById(R.id.overview_cured_text)).setText(curedCount);
 
         itemContainer.addView(item);
+    }
+
+    private String addComma(String string) {
+        DecimalFormat decimalFormat = new DecimalFormat(",###");
+        return decimalFormat.format(Double.parseDouble(string));
     }
 }
